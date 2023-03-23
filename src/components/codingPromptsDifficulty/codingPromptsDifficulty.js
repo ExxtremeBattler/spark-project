247d37623da9698bedd84e4391e72765d7dee88d
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-23 15:56:06 +0000

    started styling

--- src/components/codingPromptsDifficulty/codingPromptsDifficulty.js
+++ src/components/codingPromptsDifficulty/codingPromptsDifficulty.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import Button from '@mui/material/Button';
+import "./codingPromptsDifficulty.css"
+import Grid from '@mui/material/Grid';
 
 
 function CodingPromptsDifficulty() {
@@ -10,11 +12,29 @@ function CodingPromptsDifficulty() {
     return (
         <div className="CodingPromptsDifficulty">
 
-            <h2> Difficulty : {difficulty} </h2>
-            <Button variant = "outlined" onClick={() => setDifficulty("Beginner")}> Beginner </Button>
-            <Button variant = "outlined" onClick={() => setDifficulty("Intermediate")}> Intermediate</Button>
-            <Button variant = "outlined" onClick={() => setDifficulty("Hard")}> Hard </Button>
+            <Grid container spacing={2} columns={3}> 
 
+            <h2> Difficulty : {difficulty} </h2>
+
+            <Grid item xs={4}>
+            <div className="BeginnerButton"> 
+            <Button variant = "outlined" size = "large" color="success" onClick={() => setDifficulty("Beginner")}> Beginner </Button>
+            </div>
+            </Grid>
+
+            <Grid item xs={4}>
+            <div className="IntermediateButton">
+            <Button variant = "outlined" size = "large" onClick={() => setDifficulty("Intermediate")}> Intermediate</Button>
+            </div>
+            </Grid>
+
+            <Grid item xs={4}>
+            <div className="HardButton">
+            <Button variant = "outlined" size = "large" color = "error" onClick={() => setDifficulty("Hard")}> Hard </Button>
+            </div>
+            </Grid>
+
+            </Grid>
 
         </div>
     )

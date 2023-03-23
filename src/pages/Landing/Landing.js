2a29f7ea363b6918c4a61279cde0b9c842fca34c
NCStorey <ncsansom@hotmail.co.uk>
2023-03-23 16:44:20 +0000

    addition of header component

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainGallery from '../../components/mainGallery/mainGallery';
 import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
 
 
 function Landing() {
     return ( 
         <>
+        <Header />
         <MainGallery/>
         <Footer />
         </>

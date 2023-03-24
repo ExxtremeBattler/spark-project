23f18ab1b71434b8662dde416c3a84a9146758e3
NCStorey <ncsansom@hotmail.co.uk>
2023-03-24 14:17:43 +0000

    nav bar js

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 
 
-
 function MainButton(props) {
     return (
         <div className="App">
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,116 @@
-import React from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 
-function Header (){
+
+const pages = ['Home', 'Writing Prompts', 'Drawing Prompts', 'Coding Prompts', 'Contact'];
+
+// App bar with responsive menu copied from MUI website and altered to spark app
+function Header() {
+
+    // hook allows the menu to be opened
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    //open navbar event
+    const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);};
+
+    //close navbar event
+    const handleCloseNavMenu = () => {
+    setAnchorElNav(null);};
 
     return (
-        <div>Header</div>
-    )
-}
+    
+    
+    <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
 
-export default Header
+                {/* little icon when navbar is fully extended */}
+                <LightbulbOutlinedIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+
+                {/* box to hold hamburger menu */}
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                    >
+                    
+                    {/* hamburger menu icon */}
+                    <LightbulbOutlinedIcon sx={{ fontSize: 60 }}/>
+
+                    </IconButton>
+
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                    display: { xs: 'block', md: 'none' },
+                    }}
+                    >
+                    {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+                    </Menu>
+                </Box>
+                
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            SPARK
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } } }>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Header;

2f5b3081304a3eb999a959ffd93d1257245d75c7
jh871 <jennyh871@gmail.com>
2023-03-24 14:26:17 +0000

    prompt grid template complete

--- src/components/art-page/Body/After/PromptBody.js
+++ src/components/art-page/Body/After/PromptBody.js
@@ -6,13 +6,20 @@ import Grid from "@mui/system/Unstable_Grid/Grid";
 import './PromptBody.css'
 
 function PromptBody() {
+    // grab random drawing-prompt from json and display below
     return ( 
         <div className="pagebody">
             <Grid className="gridcontainer" container spacing={1}>
                 <Grid className="gridspace left" item xs={7}>
-                    {/* Image one Image 2 */}
+                    <div>
+                        {/* drawing prompt text */}
+                    </div>
                 </Grid>
                 <Grid className="gridspace right" item xs={4}>
+                    <div className="timeDisplay"><h3 className="timelimit">X minutes</h3>
+                    <h2 className="countdown">0:59</h2></div>
+
+                    
                     {/* selected time,
                     Countdown clock*/}
                 </Grid>

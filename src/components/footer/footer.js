132cbff6ecfaa67b47132216671bd5e270ff6d59
NCStorey <ncsansom@hotmail.co.uk>
2023-03-24 14:53:11 +0000

    footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,17 +1,19 @@
+import React from "react";
 import { Typography } from "@mui/material";
-import React, { startTransition } from "react";
-import './footer.css' 
+import './footer.css';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 function Footer (){
 
     return (
         <div className="footer">
 
-            <Typography>
-                Made with Love 
+            <Typography align='center' sx={{marginTop:'5px'}}>
+                Made with < FavoriteBorderIcon sx={{marginTop:'5px'}}/>
             </Typography>
+            
 
-            <Typography>
+            <Typography variant='h6' align='center'>
             
             ExxtremeBattler & jh871 & NCStorey 
             

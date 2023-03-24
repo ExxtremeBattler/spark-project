d21e5d12662ec30b50d612fd40f25b47d11ef010
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-24 13:09:25 +0000

    created js files and css

---
+++ src/components/codingPromptsChallenge/codingPromptsChallengeBeginner.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./codingPromptsChallenge.css"
+
+function CodingPromptsChallengeBeginner() {
+    return(
+        <div className="CodingPromptsChallengeBeginner">
+            
+            <div class="square"> 
+
+            <h2 className="challengeHeader"> challnge header </h2>
+            <p className="challengeDesc"> whassup </p>
+
+            </div>
+            
+
+        </div>
+    )
+}
+
+export default CodingPromptsChallengeBeginner

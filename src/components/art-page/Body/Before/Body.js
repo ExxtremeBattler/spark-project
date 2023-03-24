54b29a4f88a9478fd78e98e0f59c48f7c57763c0
jh871 <jennyh871@gmail.com>
2023-03-24 14:00:27 +0000

    made before and after divs same width on page

--- src/components/art-page/Body/Before/Body.js
+++ src/components/art-page/Body/Before/Body.js
@@ -8,14 +8,16 @@ import RadioBtns from "./RadioBtns";
 
 function Body() {
     return ( 
-        <Grid className="gridcontainer" container spacing={2}>
-            <Grid className="gridspace left" item xs={6}>
-                
+        <div div className="pagebody"> 
+            <Grid className="gridcontainer" container spacing={1}>
+                <Grid className="gridspace left" item xs={5.5}>
+                    
+                </Grid>
+                <Grid className="gridspace right" item xs={5.5}>
+                    <RadioBtns />
+                </Grid>
             </Grid>
-            <Grid className="gridspace right" item xs={6}>
-                <RadioBtns />
-            </Grid>
-        </Grid>
+        </div>
     );
 }
 

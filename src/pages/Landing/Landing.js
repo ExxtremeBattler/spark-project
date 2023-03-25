dc87ba082c19ea8061a06683217f1702f52b2fb5
NCStorey <ncsansom@hotmail.co.uk>
2023-03-25 20:43:44 +0000

    css fix for footer to always remain at bottom of page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -2,15 +2,19 @@ import React from 'react';
 import MainGallery from '../../components/mainGallery/mainGallery';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+import './Landing.css';
+
 
 
 function Landing() {
     return ( 
-        <>
-        <Header />
-        <MainGallery/>
-        <Footer />
-        </>
+        <div className='contentContainer'>
+            <div className='footerSpacer'>
+                <Header />
+                <MainGallery/>
+                <Footer />
+            </div>
+        </div>
      );
 }
 

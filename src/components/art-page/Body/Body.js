bd9ee06c2414b61f2adf7c5a477cff3acae569ff
jh871 <jennyh871@gmail.com>
2023-03-26 21:52:13 +0100

    added array for random time selection

--- src/components/art-page/Body/Body.js
+++ src/components/art-page/Body/Body.js
@@ -6,21 +6,32 @@
 
 import React from "react";
 import Grid from "@mui/system/Unstable_Grid/Grid";
-import './Body.css'
+
 import RadioBtns from "./RadioBtns";
 import DrawPrompts from '../../../prompts/drawing-prompts.json'
 import Timer from "./Timer";
-
+import './Body.css'
 
 function Body() {
     //if/else for setting time from radio btns - might be clearer as a switch case later;
     let time = null;
-    let selection= "5min"; //get from radio btn selection
-    if (selection === "5min") {
-        time = 300000;
-    } else if (selection === "1min") {
+    let selection= "Random"; //default
+
+    //handling random time selection;
+    const timeChoice = ['1min', '5min', '10min'];
+    let randomTime = timeChoice[Math.floor(Math.random() * timeChoice.length)];
+    if (selection === "Random") {
+        selection = randomTime;
+    };
+
+     //get from radio btn selection
+    if (selection === "1min") {
         time = 60000;
-    }
+    } else if (selection === "5min") {
+        time = 300000;
+    } else if (selection === "10min") {
+        time = 600000;
+    };
 
     //art prompt random selection
     const promptList = DrawPrompts;
@@ -29,16 +40,16 @@ function Body() {
     // console.log(artPrompt);
 
     //conditional rendering of page display
-    let prompts = "show";
+    let prompts = "hidden";
     if (prompts === "hidden"){
         return ( 
             <div div className="pagebody"> 
                 <Grid className="gridcontainer" container spacing={1}>
                     <Grid className="gridspace left" item xs={5.5}>
-                        
+
                     </Grid>
                     <Grid className="gridspace right" item xs={5.5}>
-                        <RadioBtns />
+                        <RadioBtns selection={selection}/>
                     </Grid>
                 </Grid>
             </div>

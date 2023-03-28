cbad40ecafd8e11ad30b780ea6440b638a4bcb64
jh871 <jennyh871@gmail.com>
2023-03-28 14:34:03 +0100

    renders API images to page opposite timer

--- src/components/art-page/Body/Body.js
+++ src/components/art-page/Body/Body.js
@@ -1,16 +1,11 @@
-//BEFORE - display when selecting time
-//grid - two squares; left displays question mark (placeholder for prompts), right displays radio buttons
-//AFTER - display when showing prompts and countdown
-//grid - two squares; left displays prompts, right displays timer
-
 
 import React, { useState } from "react";
 import Grid from "@mui/system/Unstable_Grid/Grid";
 import Placeholder from "./Placeholder";
 import RadioBtns from "./RadioBtns";
-import DrawPrompts from '../../../prompts/drawing-prompts.json'
 import StartBtn from "./StartBtn";
 import Timer from "./Timer";
+import Pixabay from "../../Pixabay";
 import './Body.css'
 
 function Body() {
@@ -24,15 +19,7 @@ function Body() {
     const [selection, setSelection] = useState();
     const [start, setStart] = useState(false);
 
-
-    //art prompt random selection
-    const promptList = DrawPrompts;
-    // console.log(promptList);
-    const artPrompt = promptList[Math.floor(Math.random()*promptList.length)];
-    // console.log(artPrompt);
-
     //conditional rendering of page display
-
     if (start === false){
         return ( 
             <div div className="pagebody"> 
@@ -53,7 +40,7 @@ function Body() {
                 <Grid className="gridcontainer" container spacing={1}>
                     <Grid className="gridspace left" item xs={7}>
                         <div>
-                            <h2 className="prompt-text">{artPrompt}</h2>
+                            <Pixabay />
                         </div>
                     </Grid>
                     <Grid className="gridspace right" item xs={4}>

ffa5a5ff13a3324258f0afbbdbd8408e5f8bdb5e
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-03-28 10:22:25 +0100

    landing page rendered on load

--- src/App.js
+++ src/App.js
@@ -9,26 +9,12 @@ import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom"
 
 
 function App() {
-  const [storedProjects, setStoredProjects] = useState([]);
-
-  useEffect(() => {
-  localStorage.setItem('storedProjects', JSON.stringify(storedProjects));
-}, [storedProjects]);
-
-useEffect(() => {
-  const storedProjects = JSON.parse(localStorage.getItem('storedProjects'));
-  if (storedProjects) {
-   setStoredProjects(storedProjects);
-  }
-}, []);
-  
 
   return (
   
     <Router>
     <div className="App">
-      {/* <Landing /> */}
-      <CodingPromptsDifficulty storedProjects = {storedProjects} setStoredProjects = {setStoredProjects}/>
+      <Landing /> 
       <div>
         <Routes>
           

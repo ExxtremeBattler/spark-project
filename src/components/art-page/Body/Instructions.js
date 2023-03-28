68cde85b94981561b9a4c593bb2b4586e1f6ddb8
jh871 <jennyh871@gmail.com>
2023-03-28 14:52:08 +0100

    instructions added to art page

---
+++ src/components/art-page/Body/Instructions.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Instructions() {
+    return ( 
+        <div className="instruct-div">
+            <p className="instructions">Please select yout time limit, and click 'start' to begin!
+            <br /> 
+            When the timer begins, two images will appear. Use these as prompts in your piece!
+            </p>
+        </div>
+    );
+}
+
+export default Instructions;

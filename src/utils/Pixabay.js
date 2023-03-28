c2a18a86ee1f8fa03bb69664219e63c69301ab39
jh871 <jennyh871@gmail.com>
2023-03-28 17:12:09 +0100

    corrected errors in API call path

--- src/utils/Pixabay.js
+++ src/utils/Pixabay.js
@@ -1,6 +1,5 @@
 // PIXABAY API - images
 // additional endpoints: &q={word} &safesearch=true
-
 import React, { useState, useEffect } from 'react';
 
 import setting from '../prompts/prompts-settings.json'
@@ -9,24 +8,24 @@ import subject from '../prompts/prompts-subjects.json'
 
 function Pixabay() {
     const subj = subject[Math.floor(Math.random()*subject.length)]
+    console.log(subj);
     const sett = setting[Math.floor(Math.random()*setting.length)]
+    console.log(sett);
 
     const [pic, setPic] = useState(null)
     const [picTwo, setPicTwo] = useState(null);
 
-
     useEffect(() => {
-        const setting = (selection)
-        fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABEY_API_KEY}&safesearch=true&q=`+setting)
+        fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&safesearch=true&q=${subj}`)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data.hits[0].webformatURL);
-            setPic(data.hits[0].webformatURL) //need to use math random to generate random index too maybe
+            console.log(data);
+            setPic(data.hits[0].webformatURL);
         }).then()
-        fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABEY_API_KEY}&safesearch=true&q=`+sett)
+        fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&safesearch=true&q=${sett}`)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data.hits[0].webformatURL); //need to use math random to generate random index too maybe
+            console.log(data);
             setPicTwo(data.hits[0].webformatURL)
         });
     }, [])

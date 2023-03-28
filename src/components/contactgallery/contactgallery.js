b2fb450983997550dbfd2e4d4ddff94d493a2ed8
NCStorey <ncsansom@hotmail.co.uk>
2023-03-28 14:38:48 +0100

    contactwrapper js

--- src/components/contactgallery/contactgallery.js
+++ src/components/contactgallery/contactgallery.js
@@ -1,5 +1,7 @@
 import React from "react";
-import './contactgallery.css'
+import './contactgallery.css';
+import Grid from '@mui/material/Grid';
+
 
 
 function ContactGallery() {
@@ -8,13 +10,13 @@ function ContactGallery() {
         <>
     <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
-            <div className="gridspace"><MainButton text='Writing Prompts' icon=<HistoryEduIcon sx={{ fontSize: 80 }}/> /></div>
+            <div className="gridspace"></div>
         </Grid>
         <Grid item xs={12} md={4}>
-            <div className="gridspace"><MainButton text='Drawing prompts' icon=<ColorLensIcon sx={{ fontSize: 80 }}/> /></div>
+            <div className="gridspace"></div>
         </Grid>
         <Grid item xs={12} md={4} >
-            <div className="gridspace"><MainButton text='Coding Prompts' icon=<CodeIcon sx={{ fontSize: 80 }}/> /></div>
+            <div className="gridspace"></div>
         </Grid>
     </Grid>
 
@@ -22,7 +24,7 @@ function ContactGallery() {
         <Grid item xs={12}>
         
             <div className="gridspace">
-                <LogoButton />
+                
             </div>
         </Grid>
     </Grid>

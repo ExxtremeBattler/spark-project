b10472bf3d23147e96d25a5ae8de1ea7e869dcef
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-05-25 14:42:36 +0100

    timer now rendered on all pages

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { animated, useSpring } from '@react-spring/web';
+// import { animated, useSpring } from '@react-spring/web';
 import './button.css'
 import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button, Typography} from '@mui/material';
 import { fontSize } from '@mui/system';
@@ -8,51 +8,52 @@ import { fontSize } from '@mui/system';
 
 function MainButton(props) {
 
-    const [springs, api] = useSpring(() => ({
-        from: { y: 0, rotate: 0 },
-      }))
+//     const [springs, api] = useSpring(() => ({
+//         from: { y: 0, rotate: 0 },
+//       }))
 
-      const handleHover = () => {
-        api.start({
-            loop: { reverse: false },
-            from: { y: 0, rotate: 0 },
-            to: { y: 0, rotate: 360 },
-            config: { duration: 800 }
-        })
-      }
+//       const handleHover = () => {
+//         api.start({
+//             loop: { reverse: false },
+//             from: { y: 0, rotate: 0 },
+//             to: { y: 0, rotate: 360 },
+//             config: { duration: 800 }
+//         })
+//       }
     
-    const [open, setOpen] = useState(false);
+//     const [open, setOpen] = useState(false);
 
 
 
-    return (
-        <>
-        <div>
-          <Dialog open={open} onClose={()=>setOpen(false)}>
-            <DialogTitle>
-              <Typography sx={{fontFamily:'Delicious Handrawn', fontSize:50}}>
-                {props.dialogTitleforTarget}
-              </Typography>
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                {props.dialogTextforTarget}
-              </DialogContentText>
-            </DialogContent>
-              <DialogActions>
-                <Button onClick={()=>setOpen(false)}>Close</Button>
-                <Button href={props.href} onClick={()=>setOpen(false)}>Let's Go!</Button>
-              </DialogActions>
+//     return (
+//         <>
+//         <div>
+//           <Dialog open={open} onClose={()=>setOpen(false)}>
+//             <DialogTitle>
+//               <Typography sx={{fontFamily:'Delicious Handrawn', fontSize:50}}>
+//                 {props.dialogTitleforTarget}
+//               </Typography>
+//             </DialogTitle>
+//             <DialogContent>
+//               <DialogContentText>
+//                 {props.dialogTextforTarget}
+//               </DialogContentText>
+//             </DialogContent>
+//               <DialogActions>
+//                 <Button onClick={()=>setOpen(false)}>Close</Button>
+//                 <Button href={props.href} onClick={()=>setOpen(false)}>Let's Go!</Button>
+//               </DialogActions>
 
-          </Dialog>
-        </div>
-        <animated.button style={{...springs}} onMouseEnter={handleHover} className='mainbutton' onClick={()=>setOpen(true)}>
-          <div>
-            {props.icon}
-          </div>
-            {props.text}
-        </animated.button>
-        </>
-)}
+//           </Dialog>
+//         </div>
+//         <animated.button style={{...springs}} onMouseEnter={handleHover} className='mainbutton' onClick={()=>setOpen(true)}>
+//           <div>
+//             {props.icon}
+//           </div>
+//             {props.text}
+//         </animated.button>
+//         </>
+// )
+}
 
 export default MainButton;
--- src/components/logobutton/logobutton.js
+++ src/components/logobutton/logobutton.js
@@ -1,31 +1,32 @@
 import * as React from 'react';
-import { animated, useSpring } from '@react-spring/web';
+// import { animated, useSpring } from '@react-spring/web';
 import './logobutton.css'
 
 
 
 function LogoButton(props) {
 
-    const [springs, api] = useSpring(() => ({
-        from: { y: 0, rotate: 0 },
-      }))
+//     const [springs, api] = useSpring(() => ({
+//         from: { y: 0, rotate: 0 },
+//       }))
 
-      const handleHover = () => {
-        api.start({
-            loop: { reverse: false },
-            from: { y: 0, rotate: 0 },
-            to: { y: 0, rotate: 360 },
-            config: { duration: 800 }
-        })
-      }
+//       const handleHover = () => {
+//         api.start({
+//             loop: { reverse: false },
+//             from: { y: 0, rotate: 0 },
+//             to: { y: 0, rotate: 360 },
+//             config: { duration: 800 }
+//         })
+//       }
     
 
-    return (
+//     return (
         
-        <animated.button style={{...springs}} onMouseEnter={handleHover} className='button'>
-          <div id='logobutton'></div>
-        </animated.button>
+//         <animated.button style={{...springs}} onMouseEnter={handleHover} className='button'>
+//           <div id='logobutton'></div>
+//         </animated.button>
 
-)}
+// )
+}
 
 export default LogoButton;
